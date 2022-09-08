8f0b1ac49e38389412188587e37ef2f4b1407f8b
Mendel E <mendelmaleh@gmail.com>
2022-09-08 07:07:43 -0400

    Add interactive mode

--- cmd/pattern/main.go
+++ cmd/pattern/main.go
@@ -8,34 +8,49 @@ import (
 	"time"
 
 	"git.sr.ht/~mendelmaleh/patterns"
+	"github.com/c-bata/go-prompt"
 	"github.com/kr/pretty"
 )
 
 func main() {
 	// flags
 	x := flag.Int("x", 3, "number of strings to generate")
+	i := flag.Bool("i", false, "interactive mode")
 	d := flag.Bool("d", false, "debug mode")
 	flag.Parse()
 
-	// args
-	pattern := flag.Arg(0)
-	if pattern == "" {
-		log.Fatal("no pattern")
-	}
-
-	// generator
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	g, err := patterns.NewGenerator(pattern, r)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	if *d {
-		pretty.Println(g.Tokens)
-	}
+	if *i {
+		prompt.New(
+			func(s string) {
+				g, err := patterns.NewGenerator(s, r)
+				if err != nil {
+					log.Println(err)
+					return
+				}
 
-	// generate
-	for i := 0; i < *x; i++ {
-		fmt.Println(g.Generate())
+				fmt.Println(g.Generate())
+			},
+			func(_ prompt.Document) (s []prompt.Suggest) { return },
+		).Run()
+	} else {
+		pattern := flag.Arg(0)
+		if pattern == "" {
+			log.Fatal("no pattern")
+		}
+
+		g, err := patterns.NewGenerator(pattern, r)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if *d {
+			pretty.Println(g.Tokens)
+		}
+
+		for i := 0; i < *x; i++ {
+			fmt.Println(g.Generate())
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,13 @@ go 1.19
 require github.com/kr/pretty v0.3.0
 
 require (
+	github.com/c-bata/go-prompt v0.2.6 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.7 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/mattn/go-tty v0.0.3 // indirect
+	github.com/pkg/term v1.2.0-beta.2 // indirect
 	github.com/rogpeppe/go-internal v1.6.1 // indirect
+	golang.org/x/sys v0.0.0-20200918174421-af09f7315aff // indirect
 )
